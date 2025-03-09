9690ce93102c8bbb5a5689bafff1be4e496f62c2
Aliza <alirezanajmabadi79@gmail.com>
2025-03-09 17:01:09 +0330

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=N0KERNEL Fork by AlirezaParsi
+kernel.string=ALP Kernel by AlirezaParsi
 do.devicecheck=1
 do.modules=0
 do.systemless=1
