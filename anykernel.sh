eec89336d447070386a96c713aef28d825446cb4
Aliza <alirezanajmabadi79@gmail.com>
2025-04-20 20:07:05 +0330

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -39,7 +39,7 @@ case "$ZIPFILE" in
   *-ir-5k*)   v=ir-5k;;
   *-ir*)      v=ir;;
   *-5k*)      v=5k;;
-  *N0Kernel*) v=default;;
+  *ALPKernel*) v=default;;
 esac
 
 # If none are detected (adb sideload), let the user pick
